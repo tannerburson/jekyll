4d857362e39a446e3b3ab2f777fbf523c1551512
Tanner Burson <tanner@Tanners-Laptop.local>
2010-02-09 10:18:51 -0600

    Fixed an issue with the permalinks having the full hostnames instead of just paths.

--- lib/jekyll/converters/blogger.rb
+++ lib/jekyll/converters/blogger.rb
@@ -1,7 +1,32 @@
 require 'rubygems'
-require 'fileutiles'
+require 'fileutils'
+require 'nokogiri'
+require 'uri'
 
 module Jekyll
   module Blogger
+    def self.process(filename)
+      FileUtils.mkdir_p "_posts"
+      feed = Nokogiri::XML.parse(File.open(filename)).css('feed').first
+      feed.css('entry').each do |entry|
+        if entry.children.css('category').first['term'] =~ /post/
+          title = entry.children.css('title').first.content
+          slug = entry.children.css('title').first.content
+          date = DateTime.parse(entry.children.css('published').first.content)
+          name = "%02d-%02d-%02d-%s.html" % [date.year, date.month, date.day, title]
+          data = {
+            'layout' => 'post',
+            'title' => title,
+            'permalink' => URI.split(entry.children.css('link').last['href'].to_s)[5]
+          }.to_yaml
+          content = entry.children.css('content').first.content
+          File.open("_posts/#{name}","w") do |f|
+            f.puts data
+            f.puts "---"
+            f.puts content
+          end
+        end
+      end
+    end
   end
 end
